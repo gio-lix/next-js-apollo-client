4021f49328a80e52c069b16291c5e3266d7f59de
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2022-02-17 20:13:34 +0400

    first commit

---
+++ pages/category/[slug].tsx
@@ -0,0 +1,14 @@
+import {FC} from "react"
+import {NextPage} from "next";
+import Layout from "../../components/Layout";
+
+
+const CategoriesPage: NextPage = () => {
+
+  return (
+     <Layout>
+        Categories
+     </Layout>
+  )
+}
+export default CategoriesPage
